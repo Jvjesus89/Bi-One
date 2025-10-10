1ec4392229008516ef3cc600146702aed4c0477c
Joao Vitor <89112456+Jvjesus89@users.noreply.github.com>
2025-10-10 18:21:50 -0300

    Delete ERPProjetos directory

--- ERPProjetos/components/ui/TabBarBackground.ios.js
+++
@@ -1,5 +0,0 @@
-import { BlurView } from 'expo-blur';
-
-export function TabBarBackground() {
-  return <BlurView intensity={80} style={{ flex: 1 }} />;
-} 
--- ERPProjetos/hooks/useColorScheme.js
+++
@@ -1,8 +0,0 @@
-import { useColorScheme as _useColorScheme } from 'react-native';
-
-// This hook is a wrapper around the `useColorScheme` hook from React Native.
-// It is used to get the current color scheme of the device.
-export function useColorScheme() {
-  return _useColorScheme();
-}
-
